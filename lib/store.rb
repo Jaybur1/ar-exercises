e82376665e176ea8f32b975df96420f138e6550d
Jaybur1 <jayburbyga@gmail.com>
2020-04-27 18:35:05 -0400

    exersice 7 validating entries and outputing errors

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,17 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, numericality: { greater_than: 0}
+
+  validate :apparels_validation
+
+  private
+  def apparels_validation
+    if !mens_apparel.present? && !womens_apparel.present?
+      errors.add(:mens_apparel, "Have to specify true or false")
+      errors.add(:womens_apparel, "Have to specify true or false")
+    end
+  end
+
+
 end
