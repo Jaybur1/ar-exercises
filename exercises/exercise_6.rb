83d962205cd3b0ab6ef6013d38f696a453b90e3f
Jaybur1 <jayburbyga@gmail.com>
2020-04-27 17:39:33 -0400

    exersice 6 connecting tables and creating employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,12 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Popo", last_name: "Popov", hourly_rate: 55)
+@store1.employees.create(first_name: "Pipin", last_name: "Took", hourly_rate: 45)
+
+@store2.employees.create(first_name: "Frodo", last_name: "Baggins", hourly_rate: 70)
+@store2.employees.create(first_name: "Sam", last_name: "Gamdgy", hourly_rate: 75)
+@store2.employees.create(first_name: "Gimgi", last_name: "Pimgi", hourly_rate: 41)
+
